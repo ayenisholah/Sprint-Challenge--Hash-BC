8c623656b34ac9ff0dc1acca026080126817c617
Shola Ayeni <ayenisholah@yahoo.com>
2019-11-01 11:04:11 +0100

    hash table ex 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,19 +1,25 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+# #  Hint:  You may not need all of these.  Remove the unused functions.
+from hashtables import (HashTable, hash_table_insert)
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    ht = HashTable(length)
 
-    """
-    YOUR CODE HERE
-    """
+    temp_length = 0
+    arr = []
 
-    return None
+    while temp_length < length:
+        diff = limit - weights[temp_length]
+        for (i, v) in enumerate(weights):
+            hash_table_insert(ht, v, i)
+            if diff == weights[i] and temp_length != i:
+                arr.append(i)
+
+        temp_length += 1
+
+    if len(arr) == 0:
+        return None
+    return arr
 
 
 def print_answer(answer):
